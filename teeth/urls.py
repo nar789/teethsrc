581527e249b8418375e7a6755f6ac29c0327e3dd
nar789 <tony92511@hanmail.net>
2018-03-01 19:18:26 +0900

    update url

--- teeth/urls.py
+++ teeth/urls.py
@@ -17,7 +17,8 @@ from django.conf.urls import url,include
 from django.contrib import admin
 
 urlpatterns = [
-	url(r'^teeth', include('web.urls')),
+	url(r'^$', include('web.urls')),
+	url(r'^teeth/', include('web.urls')),
     url(r'^admin/', admin.site.urls),
 ]
 
--- web/urls.py
+++ web/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import url,include
 from . import views
 
 urlpatterns = [
-	url(r'^/$', views.index,name='index'),
-	url(r'^/load', views.load,name='load'),
-	url(r'^/delete/(\d+)/', views.delete,name='delete'),
+	url(r'^$', views.index,name='index'),
+	url(r'^load', views.load,name='load'),
+	url(r'^delete/(\d+)/', views.delete,name='delete'),
 ]
 
