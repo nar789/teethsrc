680a3e55e378f4aae6ae01f0389a14ea932662a6
nar789 <tony92511@hanmail.net>
2018-03-02 23:46:17 +0900

    finish

--- web/server.py
+++ web/server.py
@@ -4,10 +4,9 @@ import sys
 # Create a TCP/IP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # Bind the socket to the port
-server_address = ('0.0.0.0', 3006)
+server_address = ('0.0.0.0', 2121)
 print >>sys.stderr, 'starting up on %s port %s' % server_address
 sock.bind(server_address)
-
 sock.listen(1)
 
 while True:
